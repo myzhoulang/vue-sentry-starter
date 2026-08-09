067d442dccb0c201e62987bcf8769ca27a2ce97e
myzhoulang <7009272+myzhoulang@users.noreply.github.com>
2026-08-09 21:17:28 +0800

    新增类型错误按钮用于 Sentry 异常上报测试

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <p class="eyebrow">Vue 3 · TypeScript · Vite</p>
     <h1>一个简洁的 Vue 起点</h1>
     <p class="lead">路由和 Sentry 已配置完毕，可以直接开始开发。</p>
+    <button class="button" type="button" @click="onClick2">点击</button>
     <RouterLink class="button" to="/about">查看项目配置</RouterLink>
     <button class="button" type="button" @click="onClick">触发测试错误</button>
   </section>
@@ -13,4 +14,11 @@
 const onClick = (): never => {
   throw new Error('Sentry 前端测试错误')
 }
+
+
+const onClick2 = () => {
+  // @ts-ignore
+  console.log(window.a.b);
+}
+
 </script>
